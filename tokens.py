b02c19c7af120dce64de5324a9d23551497727d9
Tycho Grouwstra <tychogrouwstra@gmail.com>
2019-11-25 15:42:05 +0100

    type tokens

--- tokens.py
+++ tokens.py
@@ -1,74 +1,76 @@
+"""extract desired tokens from a string"""
+from typing import List
 import sys
 import re
 
 alphaReg = re.compile(r'^[a-zA-Z]+$')
-def isalpha(s):
+def isalpha(s: str) -> bool:
     return alphaReg.match(s) is not None
 
-def NumTok(string):
+def NumTok(string: str) -> List[str]:
 	node = re.findall(r"[0-9]+" , string)
 	return node
 
-def AlphaTok(string):
+def AlphaTok(string: str) -> List[str]:
 	node = re.findall(r"[a-zA-Z]+" , string)
 	return node
 
-def SpaceTok(string):
+def SpaceTok(string: str) -> List[str]:
 	node = [" "]
 	return node
 
-def PeriodTok(string):
+def PeriodTok(string: str) -> List[str]:
 	node = ["."]
 	return node
 
-def CommaTok(string):
+def CommaTok(string: str) -> List[str]:
 	node = [","]
 	return node
 
-def LeftParenthesisTok(string):
+def LeftParenthesisTok(string: str) -> List[str]:
 	node = ["("]
 	return node
 
-def RightParenthesisTok(string):
+def RightParenthesisTok(string: str) -> List[str]:
 	node = [")"]
 	return node
 
-def DQuoteTok(string):
+def DQuoteTok(string: str) -> List[str]:
 	node = ["\""]
 	return node
 
-def SQuoteTok(string):
+def SQuoteTok(string: str) -> List[str]:
 	node = ["'"]
 	return node
 
-def HyphenTok(string):
+def HyphenTok(string: str) -> List[str]:
 	node = ["-"]
 	return node
 
-def UBarTok(string):
+def UBarTok(string: str) -> List[str]:
 	node = ["_"]
 	return node
 
-def SlashTok(string):
+def SlashTok(string: str) -> List[str]:
 	node = ["/"]
 	return node
 
-def StartTok(string):
+def StartTok(string: str) -> List[str]:
 	node = string[0]
 	return node
 
-def EndTok(string):
+def EndTok(string: str) -> List[str]:
 	node = string[-1]
 	return node
 
-def EOFTok(string):
+def EOFTok(string: str) -> List[str]:
 	node = [""]
 	return node
 
-def NoneTok(string):
+def NoneTok(string: str) -> List[str]:
 	node = [None]
 	return node
 
-def MatchTok(string):
+def MatchTok(string: str) -> List[str]:
 	node = [string]
 	return node
